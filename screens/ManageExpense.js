92915959c8fd242261e8807c2cc53755dc4ad32d
Furqan Masood <bhatfurqan@gmail.com>
2024-07-23 22:57:58 +0530

    Editing ManageExpense Screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,15 @@
-import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { useCallback, useLayoutEffect } from "react";
+import { Button, StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 const ManageExpense = ({ route, navigation }) => {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
+
+  const deleteExpenseHandler = useCallback(() => {
+    navigation.goBack();
+  }, []);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -11,7 +17,35 @@ const ManageExpense = ({ route, navigation }) => {
     });
   }, [navigation, isEditing]);
 
-  return <Text>Manage Expense Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.deleteContainer}>
+        {isEditing && (
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        )}
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
 
 export default ManageExpense;
