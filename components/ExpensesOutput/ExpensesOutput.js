80899865736656d09109c5fce35be9856298e2f5
Furqan Masood <bhatfurqan@gmail.com>
2024-07-24 00:56:44 +0530

    App Completed [Static Addition]

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,15 +1,20 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
 const ExpensesOutput = (props) => {
-  const { expenses, expensesPeriod } = props;
+  const { expenses, expensesPeriod, fallbackText } = props;
 
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+  }
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList expenses={expenses} />
+      {content}
     </View>
   );
 };
@@ -23,5 +28,11 @@ const styles = StyleSheet.create({
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
     flex: 1,
+  },
+  infoText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
